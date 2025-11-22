e34dd6a2c4d9bd3e68727a4954b04886ccbc1e13
Prince95-cmd <obiekezieprincec@gmail.com>
2025-11-23 00:45:46 +0100

    first commit

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express, {Request, Response, NextFunction} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 10000;
+
+interface AppError extends Error {
+    status?: number;
+}
+
+app.get('/', (_req: Request, res: Response) => {
+    res.send('Payment, Payout, and Refund integration');
+    console.log('Payment, Payout, and Refund integration')
+});
+
+app.use((err: AppError, _req: Request, res: Response, _next: NextFunction)=>{
+    console.error(err);
+    res.status(err.status || 500)
+    res.json({
+        error: err.message || 'Internal Server Error'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
